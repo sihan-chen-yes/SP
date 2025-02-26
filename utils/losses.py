67223d252e9b906c84364f746580135dc10c2c36
Sihan Chen <sihanchen2001222@gmail.com>
2025-02-26 15:29:19 +0100

    change depth smoothing method

--- utils/losses.py
+++ utils/losses.py
@@ -203,23 +203,22 @@ def depth_map_smooth_loss(depth_map, mask, alpha=10):
     depth_map: (H, W, 1)
     """
 
-    grad_x = torch.abs(mask[:, 1:] - mask[:, :-1])  # shape: (H, W-1)
-    grad_y = torch.abs(mask[1:, :] - mask[:-1, :])  # shape: (H-1, W)
-
-    grad_x_crop = grad_x[1:, :]  # (H-1, W-1)
-    grad_y_crop = grad_y[:, 1:]  # (H-1, W-1)
-
-    # grad = torch.sqrt(torch.square(grad_x_crop) + torch.square(grad_y_crop))
-    grad = torch.sqrt(torch.clamp(torch.square(grad_x_crop) + torch.square(grad_y_crop), min=1e-6))
-
-    # grad_disp = (grad.detach().cpu().numpy() * 255).astype(np.uint8)
-    # cv.imshow("Grad Map", grad_disp)
-    # cv.waitKey(0)
-    # cv.destroyAllWindows()
-
-    weight = torch.exp(-alpha * grad) # (H-1, W-1)
-    # weight = torch.exp(-alpha * grad) * mask[:-1, :-1] # (H-1, W-1)
-    weight_crop = weight[1:, 1:]
+    # grad_x = torch.abs(mask[:, 1:] - mask[:, :-1])  # shape: (H, W-1)
+    # grad_y = torch.abs(mask[1:, :] - mask[:-1, :])  # shape: (H-1, W)
+    #
+    # grad_x_crop = grad_x[1:, :]  # (H-1, W-1)
+    # grad_y_crop = grad_y[:, 1:]  # (H-1, W-1)
+    #
+    # grad = torch.sqrt(torch.clamp(torch.square(grad_x_crop) + torch.square(grad_y_crop), min=1e-6))
+    #
+    # # grad_disp = (grad.detach().cpu().numpy() * 255).astype(np.uint8)
+    # # cv.imshow("Grad Map", grad_disp)
+    # # cv.waitKey(0)
+    # # cv.destroyAllWindows()
+    #
+    # weight = torch.exp(-alpha * grad) # (H-1, W-1)
+    # # weight = torch.exp(-alpha * grad) * mask[:-1, :-1] # (H-1, W-1)
+    # weight_crop = weight[1:, 1:]
 
     # Laplacian computation (second order gradient)
     lap_x = depth_map[:, :-2] - 2 * depth_map[:, 1:-1] + depth_map[:, 2:] # (H, W-2)
@@ -228,9 +227,15 @@ def depth_map_smooth_loss(depth_map, mask, alpha=10):
     lap_x_crop = lap_x[1:-1, :]  # (H-2, W-2)
     lap_y_crop = lap_y[:, 1:-1]  # (H-2, W-2)
     #TODO
-    loss_x = (weight_crop * torch.abs(lap_x_crop)).mean()
-    loss_y = (weight_crop * torch.abs(lap_y_crop)).mean()
+    # loss_x = (weight_crop * torch.abs(lap_x_crop)).mean()
+    # loss_y = (weight_crop * torch.abs(lap_y_crop)).mean()
 
-    return loss_x + loss_y
+    # loss = weight_crop * torch.abs(lap_x_crop) + weight_crop * torch.abs(lap_y_crop)
+    loss = torch.abs(lap_x_crop) + torch.abs(lap_y_crop)
+    # foreground mask
+    mask_crop = mask[1:-1, 1:-1]
+    loss = loss[mask_crop >= 0.5]
+
+    return loss.mean()
 
 
