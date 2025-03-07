584f03b9b886edde34ca20dbfcf11c2c881ee731
Sihan Chen <sihanchen2001222@gmail.com>
2025-03-07 19:43:38 +0100

    fix: change supervision opacity value

--- gaussians/gaussian_model.py
+++ gaussians/gaussian_model.py
@@ -203,8 +203,8 @@ class GaussianModel:
         rots[:, 0] = 1
 
         # opacities = inverse_sigmoid(0.1 * torch.ones((fused_point_cloud.shape[0], 1), dtype=torch.float, device="cuda"))
-        opacities = inverse_sigmoid(0.9 * torch.ones((fused_point_cloud.shape[0], 1), dtype=torch.float, device="cuda"))
-        opacities[~mask.flatten()] = inverse_sigmoid(torch.tensor(0.1, dtype=torch.float, device="cuda"))
+        opacities = inverse_sigmoid(0.99 * torch.ones((fused_point_cloud.shape[0], 1), dtype=torch.float, device="cuda"))
+        opacities[~mask.flatten()] = inverse_sigmoid(torch.tensor(0.01, dtype=torch.float, device="cuda"))
 
         self._xyz = nn.Parameter(fused_point_cloud.requires_grad_(True))
         self._features_dc = nn.Parameter(features[:,:,0:1].transpose(1, 2).contiguous().requires_grad_(True))
